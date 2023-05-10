f53b0a96c6ed8fcc14ef7f667af19ddc333164e9
sangyeopKim <creativethony@naver.com>
2023-05-10 22:23:49 +0900

    ê°root url

--- src/main/java/com/ll/sbb/MainController.java
+++ src/main/java/com/ll/sbb/MainController.java
@@ -6,7 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @GetMapping("/main")
-        public String main(){
-            return  "As";
-        }
+    public String main() {
+        return "As";
+    }
+
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/question_list";
+        // redirect사용하면 바로 mapping한 주소로 가고 그에 맞는 템플릿 보여줌
+    }
 }
