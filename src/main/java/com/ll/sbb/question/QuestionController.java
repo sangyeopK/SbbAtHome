bb8a50a0054c498c8bccba4f239ef60300215b52
sangyeopKim <creativethony@naver.com>
2023-05-12 00:24:45 +0900

    질문 등록 추가 question_form새로 만들어서

--- src/main/java/com/ll/sbb/question/QuestionController.java
+++ src/main/java/com/ll/sbb/question/QuestionController.java
@@ -1,16 +1,17 @@
 package com.ll.sbb.question;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @RequiredArgsConstructor
+//생성자 주입임 그런데 final키가 붙음
 @Controller
 public class QuestionController {
     private final QuestionService questionService;
@@ -20,7 +21,8 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         // DB에서 question에 대한 데이터를 가져와서 questionlist에 넣어준다
         model.addAttribute("questionList", questionList);
-        //model은 기본적으로 뷰단에 뿌리기 위한 수단으로써 사용하는데 html에서 사용할 이름과, 실제로 받는 변수명 등을 순서대로 입력해주어야 한다.
+        //model은 기본적으로 뷰 단에 뿌리기 위한 수단으로써 사용하는데 html에서 사용할 이름과, 실제로 받는 변수명 등을 순서대로 입력해주어야 한다.
+        // 쉽게 말해 DB데이터 화면에 보여주고 싶을 때 사용하는거라고 생각하면 편할듯
         return "question_list";
     }
 
@@ -32,5 +34,16 @@ public class QuestionController {
         model.addAttribute("question", question);
         // question_detail에서 찾아 쓸 question이라는 것.
         return "question_detail";
+    }
+
+    @GetMapping("/question/create")
+    public String questionCreate(){
+        return "question_form";
+    }
+
+    @PostMapping("/question/create")
+    public String questionCreate(@RequestParam String title, @RequestParam String content){
+        questionService.create(title, content);
+        return "redirect:/question/list";
     }
 }
--- src/main/java/com/ll/sbb/question/QuestionService.java
+++ src/main/java/com/ll/sbb/question/QuestionService.java
@@ -3,6 +3,7 @@ package com.ll.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,15 +14,24 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+        // 질문 목록 보려고
     }
 
     public Question getQuestion(Integer id) {
         Optional<Question> question = this.questionRepository.findById(id);
-        // question의 id로 찾는 question.
+        // question의 id로 찾는 질문에 해당하는 데이터들 찾는겨.
         if(question.isPresent()){
             return question.get();
         } else {
             return null;
         }
+    }
+
+    public void create(String title, String content){
+        Question q = new Question();
+        q.setTitle(title);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }
